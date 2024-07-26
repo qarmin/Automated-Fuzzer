af57580183e814ae09f9fb844971f2f05d8920b0
Rafał Mikrut <mikrutrafal@protonmail.com>
2024-07-26 23:27:27 +0200

    Test

---
+++ src/crates/lofty_afl/src/main.rs
@@ -0,0 +1,60 @@
+use std::env::args;
+use std::fs::File;
+
+use lofty::file::AudioFile;
+use lofty::file::TaggedFileExt;
+use lofty::read_from;
+use std::path::Path;
+use walkdir::WalkDir;
+
+fn main() {
+    let path = args().nth(1).unwrap().clone();
+    if !Path::new(&path).exists() {
+        panic!("Missing file");
+    }
+
+    if Path::new(&path).is_dir() {
+        for entry in WalkDir::new(&path).into_iter().flatten() {
+            if !entry.file_type().is_file() {
+                continue;
+            }
+            let path = entry.path().to_string_lossy().to_string();
+            check_file(&path);
+        }
+    } else {
+        check_file(&path);
+    }
+}
+
+fn check_file(path: &str) {
+    let mut file = match File::open(&path) {
+        Ok(t) => t,
+        Err(e) => {
+            println!("{e} - {path}");
+            return;
+        }
+    };
+    let file_content = match std::fs::read(&path) {
+        Ok(t) => t,
+        Err(e) => {
+            println!("{e} - {path}");
+            return;
+        }
+    };
+
+    check_bytes(&file_content);
+}
+
+fn check_bytes(bytes: &[u8]) {
+    let tagged_file = match read_from(&mut bytes) {
+        Ok(t) => t,
+        Err(e) => {
+            println!("{e}");
+            return;
+        }
+    };
+
+    tagged_file.properties();
+    tagged_file.tags();
+    tagged_file.primary_tag();
+}
