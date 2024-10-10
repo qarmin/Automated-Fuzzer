549a86a239de3cdab51a7ea868795d28e0247ec7
Rafał Mikrut <mikrutrafal@protonmail.com>
2024-10-10 14:16:25 +0200

    Do not use too much brackets

--- fuzz/fuzz_targets/slint.rs
+++ fuzz/fuzz_targets/slint.rs
@@ -6,6 +6,16 @@ use libfuzzer_sys::{fuzz_target, Corpus};
 
 fuzz_target!(|data: &[u8]| -> Corpus {
     if let Ok(s) = std::str::from_utf8(data) {
+        let mut open_brackets = 0;
+        for b in s.bytes() {
+            if b == b'{' {
+                open_brackets += 1;
+                if open_brackets > 200 {
+                    return Corpus::Reject;
+                }
+            }
+        }
+
         let mut b = BuildDiagnostics::default();
         parse(s.to_string(), None, &mut b);
         // if lexer::lex(s).is_empty() {
