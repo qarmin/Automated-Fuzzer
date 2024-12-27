9f5e40fc516fd42c2e42408d525ff70d80a622f4
Rafał Mikrut <mikrutrafal@protonmail.com>
2024-12-27 09:13:08 +0100

    ALL

--- justfile
+++ justfile
@@ -2,6 +2,9 @@ upgrade:
     cargo +nightly -Z unstable-options update --breaking
     cargo update
 
+update_all:
+    for dir in */; do (cd "$dir" && cargo update); done
+
 fix:
     cargo +nightly fmt
     cargo clippy --fix --allow-dirty --allow-staged -- -Wclippy::pedantic -Aclippy::default_trait_access -Aclippy::cast_possible_truncation -Aclippy::must_use_candidate -Aclippy::missing_panics_doc -Aclippy::too_many_lines -Aclippy::cast_precision_loss -Aclippy::cast_sign_loss -Aclippy::module_name_repetitions -Aclippy::struct_excessive_bools -Aclippy::cast_possible_wrap -Aclippy::cast_lossless -Aclippy::if_not_else -Aclippy::wildcard_imports -Aclippy::return_self_not_must_use -Aclippy::missing_errors_doc -Aclippy::match_wildcard_for_single_variants -Aclippy::assigning_clones -Aclippy::unused_self -Aclippy::manual_is_variant_and -Aclippy::new_without_default
---
+++ src/crates/vidduplicate/src/main.rs
@@ -0,0 +1,35 @@
+use std::env::args;
+use std::fs;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use vid_dup_finder_lib::ffmpeg_builder;
+use walkdir::WalkDir;
+
+fn main() {
+    let path = args().nth(1).unwrap().clone();
+    if !Path::new(&path).exists() {
+        panic!("Missing file, {path:?}");
+    }
+
+    if Path::new(&path).is_dir() {
+        for entry in WalkDir::new(&path).into_iter().flatten() {
+            if !entry.file_type().is_file() {
+                continue;
+            }
+            let path = entry.path().to_string_lossy().to_string();
+            check_file(&path);
+        }
+    } else {
+        check_file(&path);
+    }
+}
+fn check_file(file_path: &str) {
+    println!("Checking file: {:?}", file_path);
+    let _vhash = match ffmpeg_builder::VideoHashBuilder::default().hash(PathBuf::from(file_path)) {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("Error while hashing file: {:?}", e);
+            return;
+        }
+    };
+}
