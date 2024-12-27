4d7e8e7927266e88ed352444f2786695c7320d3a
Rafał Mikrut <mikrutrafal@protonmail.com>
2024-12-27 13:51:13 +0100

    Symphonia

--- src/crates/symphonia/src/main.rs
+++ src/crates/symphonia/src/main.rs
@@ -3,10 +3,13 @@ use std::{fs, io};
 
 use walkdir::WalkDir;
 use std::path::Path;
-use symphonia::core::codecs::CODEC_TYPE_NULL;
+use symphonia::core::codecs::{CodecParameters};
+use symphonia::core::codecs::audio::{AudioDecoderOptions};
 use symphonia::core::errors::Error;
-use symphonia::core::errors::Error::IoError;
+use symphonia::core::formats::{FormatOptions, FormatReader, TrackType};
+use symphonia::core::formats::probe::Hint;
 use symphonia::core::io::MediaSourceStream;
+use symphonia::core::meta::MetadataOptions;
 
 fn main() {
     let path = args().nth(1).unwrap().clone();
@@ -37,58 +40,69 @@ fn check_file(path: &str) {
     };
 }
 
-pub fn parse_audio_file(content: Vec<u8>) -> Result<(), Error> {
+pub fn parse_audio_file(content: Vec<u8>) -> Result<(), String> {
     let cursor = io::Cursor::new(content);
     let mss = MediaSourceStream::new(Box::new(cursor), Default::default());
+    let fmt_opts =        FormatOptions {enable_gapless: true,  ..Default::default()};
+    let meta_opts: MetadataOptions = Default::default();
+    let  hint = Hint::new();
 
-    let probed = match symphonia::default::get_probe().format(
-        &Default::default(),
-        mss,
-        &Default::default(),
-        &Default::default(),
-    ) {
-        Ok(t) => t,
-        Err(_e) => {
-            return Err(Error::Unsupported(
-               "probe info not available/file not recognized",
-            ))
+    let probed = match symphonia::default::get_probe().probe(&hint, mss, fmt_opts, meta_opts) {
+        Ok(format) => {
+            format
+        }
+        Err(e) => {
+            return Err(e.to_string());
         }
     };
 
-    let mut format = probed.format;
+    let opts = DecodeOptions {
+        decoder_opts: AudioDecoderOptions { verify: true, ..Default::default() },
+    };
+    decode_only(probed, opts)?;
 
-    let track = match format
-        .tracks()
-        .iter()
-        .find(|t| t.codec_params.codec != CODEC_TYPE_NULL)
-    {
-        Some(k) => k,
-        None => return Err(Error::Unsupported("not supported audio track")),
+    Ok(())
+}
+
+#[derive(Copy, Clone)]
+struct DecodeOptions {
+    decoder_opts: AudioDecoderOptions,
+}
+
+fn decode_only(mut reader: Box<dyn FormatReader>, opts: DecodeOptions) -> Result<(), String> {
+    let track = reader.default_track(TrackType::Audio);
+
+    let track = match track {
+        Some(track) => track,
+        _ => return Ok(()),
+    };
+
+    let codec_params = match track.codec_params.as_ref() {
+        Some(CodecParameters::Audio(audio)) => audio,
+        _ => return Ok(()),
     };
 
     let mut decoder =
-        match symphonia::default::get_codecs().make(&track.codec_params, &Default::default()) {
-            Ok(k) => k,
-            Err(_) => return Err(Error::Unsupported("not supported codec")),
-        };
+        symphonia::default::get_codecs().make_audio_decoder(codec_params, &opts.decoder_opts).map_err(|e| e.to_string())?;
+
+    let track_id = track.id;
 
     loop {
-        let packet = match format.next_packet() {
-            Ok(packet) => packet,
-            Err(Error::ResetRequired) => {
-                return Err(Error::ResetRequired);
-            }
-            Err(err) => {
-                if let IoError(ref er) = err {
-                    // Catch eof, not sure how to do it properly
-                    if er.kind() == io::ErrorKind::UnexpectedEof {
-                        return Ok(());
-                    }
-                }
-                return Err(err);
-            }
+        let Some(packet) = reader.next_packet().map_err(|e|e.to_string())?
+        else {
+            break;
         };
 
-        decoder.decode(&packet)?;
+        if packet.track_id() != track_id {
+            continue;
+        }
+
+        match decoder.decode(&packet) {
+            Ok(_decoded) => continue,
+            Err(Error::DecodeError(err)) => eprintln!("decode error: {}", err),
+            Err(err) => return Err(err.to_string()),
+        }
     }
+
+    Ok(())
 }
