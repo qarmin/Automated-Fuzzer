7b8be29a24aee9bf09f0d835027e21e8803d00e6
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-07-01 23:50:55 +0200

    Buffalo

---
+++ fuzz/fuzz_targets/hayro.rs
@@ -0,0 +1,14 @@
+#![no_main]
+
+use hayro_render::render_png;
+use hayro_syntax::pdf::Pdf;
+use libfuzzer_sys::{Corpus, fuzz_target};
+use std::io::Read;
+use std::sync::Arc;
+
+fuzz_target!(|data: &[u8]| -> Corpus {
+    if let Some(pdf) = Pdf::new(Arc::new(data.to_vec())) {
+        let _pixmaps = render_png(&pdf, 1.0, None).unwrap();
+    }
+    Corpus::Keep
+});
